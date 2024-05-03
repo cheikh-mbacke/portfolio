c2a342ef26044d778d5926ea88e0e5cdfd49c47f
Cheikh Mbacké <mbackecheikhouna7@gmail.com>
2024-05-03 11:08:15 +0200

    correction route portfolio

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -31,7 +31,7 @@ export default class Main extends Component {
             render={(props) => <Home {...props} theme={this.props.theme} />}
           />
           <Route
-            path="/portolio"
+            path="/portfolio"
             render={(props) => <Home {...props} theme={this.props.theme} />}
           />
           <Route
